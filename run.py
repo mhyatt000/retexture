addd034cc9b386e3a0849a8da57539f55887e137
Matthew Hyatt <mhyatt000@gmail.com>
2023-11-20 12:32:41 -0600

    deploy

--- run.py
+++ run.py
@@ -3,11 +3,16 @@ import json
 import os
 import os.path as osp
 import time
+from concurrent.futures import ProcessPoolExecutor
 
 from omegaconf import OmegaConf
 from tqdm import tqdm
 
 ROOT = osp.dirname(__file__)
+
+config_path = osp.join(ROOT, "configs", "base.json")
+cfg = OmegaConf.load(config_path)
+print(cfg)
 
 
 def load_data(cfg):
@@ -40,26 +45,51 @@ def cfg2cmd(config):
     return " ".join(args)
 
 
+def basename(file):
+    """return filename with no extensions"""
+    return osp.basename(file).split(".")[0]
+
+
+def render(pair):
+    """renders a model & texture pair"""
+
+    model, texture = pair
+    quiet = " > /dev/null 2>&1"
+    blend = f"blender -b -P ~/cs/retexture/retexture/main.py -- {cfg2cmd(cfg)}"
+    model_args = " " + cfg2cmd({"model": model, "texture": texture})
+    os.system(blend + model_args + quiet)
+
+
+def pair_complete(pair):
+    """skip the ones that have been made"""
+
+    model, texture = pair
+    out_pair = osp.join(ROOT, cfg.out_dir, basename(model), basename(texture))
+    if osp.exists(out_pair) and len(os.listdir(out_pair)) >= cfg.nangles:
+            return True
+    return False
+
+
 def main():
-    config_path = osp.join(ROOT, "configs", "base.json")
-    cfg = OmegaConf.load(config_path)
-    print(cfg)
 
     # shell commands
     quiet = " > /dev/null 2>&1"
-    blend = f"blender -b -P ~/cs/retexture/retexture/main.py -- {cfg2cmd(cfg)}"
     media = "python ~/cs/retexture/retexture/util/walk_media.py"
 
-    os.system(f"rm -r {cfg.out_dir} + {quiet}")
+    # if input(f"delete {cfg.out_dir}? (y/n) ") == "y":
+        # print("deleting {cfg.out_dir}...")
+        # os.system(f"rm -r {cfg.out_dir} + {quiet}")
 
     models, textures = load_data(cfg)
     pairs = list(itertools.product(models, textures))
+    pairs = [p for p in pairs if  not pair_complete(p) ] 
 
-    for model, texture in tqdm(pairs, desc="Rendering 3D Models..."):
-        model_args = ' ' + cfg2cmd({"model": model, "texture": texture})
-        os.system(blend + model_args + quiet)
+    with ProcessPoolExecutor() as ex:
+        desc = "Rendering 3D Models..."
+        for _ in tqdm( ex.map(render, pairs), total=len(pairs), desc=desc):
+            pass
 
-    os.system(media)
+    # os.system(media)
 
 
 if __name__ == "__main__":
