57d542b87713f7e3f8b92dfa9ee0d74e2dcad8ab
Matthew Hyatt <mhyatt000@gmail.com>
2023-12-06 16:02:48 -0600

    first test

---
+++ tests/test_dataset.py
@@ -0,0 +1,12 @@
+import unittest
+from retexture.utils.dataset_utils import RetextureDataset as RTD
+
+class TestDataset(unittest.TestCase):
+
+    def test_func(self):
+        params = "TODO"
+        result = "TODO"
+        self.assertEqual(RTD.func(params), result)
+
+if __name__ == '__main__':
+    unittest.main()
