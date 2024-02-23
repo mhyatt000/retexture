19a4b4c436b8026c79b8590b358da8cb0de17385
Matthew Hyatt <mhyatt000@gmail.com>
2024-02-23 12:29:02 -0600

    analysis

---
+++ viewpoint.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+
+from matplotlib.colors import LogNorm
+import numpy as np
+import pandas as pd
+import requests
+import seaborn as sns
+
+
+class DataFrameHeatMap:
+    def __init__(self, file_path):
+        self.df = pd.read_csv(file_path)
+        self.mk_imagenet_map()
+
+    def process_data(self):
+
+        self.df['viewpoint'] = self.df['path'].apply(lambda x: x.split("/")[-1].split("_")[-1].split(".")[0])
+        self.df['model'] = self.df['path'].apply(lambda x: x.split("/")[-1].split("_")[0])
+        self.df['texture'] = self.df['path'].apply(lambda x: x.split("/")[-1].split("_")[1])
+
+        out_columns = [f"out{i}" for i in range(1000)]  # Adjust range if necessary
+
+        for  (model,tex), sf in self.df.groupby(['model','texture']):
+            print(model,tex)
+
+            for view , ssf in sf.groupby('viewpoint'):
+                print(view)
+
+                outs = ssf[out_columns].sum().tolist()
+                plt.bar([i for i in range(len(outs))], outs)
+                plt.title(f'{model} {tex} {view}')
+                plt.ylim(0,1)
+                plt.show()
+
+            # sf['top5'] = sf[out_columns].apply(lambda x: np.argsort(x)[-5:])
+            quit()
+        
+        print(self.df['viewpoint'].unique())
+        print(self.df['model'].unique())
+        print(self.df['texture'].unique())
+        quit()
+
+        # Assuming 'gt' needs to be one-hot encoded. If it's already, adjust accordingly.
+        gt_one_hot = pd.get_dummies(self.df["gt"], prefix="gt")
+
+        # Aggregate 'out' columns by 'gt' values
+        out_columns = [f"out{i}" for i in range(1000)]  # Adjust range if necessary
+        self.df[out_columns] = self.df[out_columns]
+        aggregated_data = self.df[out_columns].groupby(self.df["gt"]).sum()
+
+        data = aggregated_data.to_numpy().T
+        return data
+
+    def get_paths_by_gt(self):
+        paths_by_gt = {}
+        for gt in self.df["gt"].unique():
+            paths = self.df[self.df["gt"] == gt]["path"].tolist()
+            paths_by_gt[gt] = paths[0].split("/")[-1].split("_")[0]
+        return paths_by_gt
+
+    def generate_heat_map(self, heatmap_data):
+        plt.figure(figsize=(10, 8))
+
+        models = self.get_paths_by_gt()
+        x_labels = [models[i] for i in range(1, 99)]
+        y_labels = [self.imagenet_classes[i] for i in range(1, 1000)]
+
+        plt.figure(
+            figsize=(20, 10)
+        )  # You might need to adjust this depending on your screen and readability
+        sns.heatmap(
+            heatmap_data,
+            xticklabels=x_labels,
+            yticklabels=y_labels,
+            cmap="YlGnBu",
+            norm=LogNorm(vmin=heatmap_data.min().min(), vmax=heatmap_data.max().max()),
+        )
+
+        plt.xticks(rotation=45)  # Rotate x-axis labels to avoid overlap
+        plt.yticks(rotation=0)  # Ensure y-axis labels are horizontal for readability
+
+        plt.xlabel("Model Classes")
+        plt.ylabel("Predicted Classes")
+
+        plt.show()
+
+    def mk_imagenet_map(self):
+        LABELS_URL = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
+
+        response = requests.get(LABELS_URL)
+        class_idx = response.json()
+        self.imagenet_classes = {int(key): value[1] for key, value in class_idx.items()}
+
+
+path = "resnet.csv"
+# path = "res_small.csv"
+heatmap_generator = DataFrameHeatMap(path)
+heatmap_data = heatmap_generator.process_data()
+heatmap_generator.generate_heat_map(heatmap_data)
